62b9a42576c6bd0b3d9fe4062390d831e5d0fc38
lrg1031@live.com <>
2021-09-13 21:01:49 -0400

    the is my first commit form my home computer

---
+++ Luke_Green_Lab_01/src/main/java/com/mycompany/luke_green_lab_01/Lab01.java
@@ -0,0 +1,13 @@
+package com.mycompany.luke_green_lab_01;
+
+
+public class Lab01 {
+    
+    public static void main(String[] args) {
+        //i decided to go with a for loop for my implementation of lab 01
+        for(int i=0; i<=100; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+    
+}
